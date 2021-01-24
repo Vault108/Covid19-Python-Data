d8b4fe4e388f7679f70f2337020dd85c7fb5ad63
Vault108 <Vault108@users.noreply.github.com>
2021-01-24 01:27:27 +0000

    autopep8 action fixes

--- C19.py
+++ C19.py
@@ -5,19 +5,41 @@ from rich import print
 from rich.table import Table
 from rich.console import Console
 CC = Console().input("[blue]Enter a Country Code: ")
-Name = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["name"]
-Population = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["population"]
-Today_Confirmed = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["today"]["confirmed"]
-Today_Deaths = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["today"]["deaths"]
-Latest_Deaths = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["deaths"]
-Latest_Confirmed = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["confirmed"]
-Latest_Recovered = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["recovered"]
-Latest_Critical = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["critical"]
-Death_rate = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["calculated"]["death_rate"]
-Recovery_rate = requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["latest_data"]["calculated"]["recovery_rate"]
-Last_updated =  requests.get('https://corona-api.com/countries/'+ CC).json()["data"]["updated_at"]
+Name = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["name"]
+Population = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["population"]
+Today_Confirmed = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["today"]["confirmed"]
+Today_Deaths = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["today"]["deaths"]
+Latest_Deaths = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["deaths"]
+Latest_Confirmed = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["confirmed"]
+Latest_Recovered = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["recovered"]
+Latest_Critical = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["critical"]
+Death_rate = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["calculated"]["death_rate"]
+Recovery_rate = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["latest_data"]["calculated"]["recovery_rate"]
+Last_updated = requests.get(
+    'https://corona-api.com/countries/' +
+    CC).json()["data"]["updated_at"]
 
-print("[bold blue]Stats For: " +  Name )
+print("[bold blue]Stats For: " + Name)
 print("[blue]Total Population:", Population)
 print("[blue]Cases Confirmed Today:", Today_Confirmed)
 print("[blue]Deaths Today:", Today_Deaths)
@@ -25,6 +47,6 @@ print("[blue]Latest Deaths:", Latest_Deaths)
 print("[blue]Latest Confirmed:", Latest_Confirmed)
 print("[blue]Latest Recovered:", Latest_Recovered)
 print("[blue]Latest Critial:", Latest_Critical)
-print ("[blue]Death Rate: {}%".format(Death_rate))
+print("[blue]Death Rate: {}%".format(Death_rate))
 print("[blue]Recovery_rate: {}%".format(Recovery_rate))
 print("[blue]Last Updated: " + Last_updated)
